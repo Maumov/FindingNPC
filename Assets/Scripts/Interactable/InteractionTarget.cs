d9b92adb314fe2c3ed13f67e365dd7a53f0c097f
Insidios <mau.movilla@gmail.com>
2021-03-27 17:50:32 -0500

    Interactables
    - Interface de interaccion
    - Mecanica de jugador de interaccion
    - NPC interaccion

---
+++ Assets/Scripts/Interactable/InteractionTarget.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionTarget : MonoBehaviour, IInteractable
+{
+    public virtual void Interact() {
+        
+    }
+}
--- Assets/Scripts/Interactable/PlayerInteraction.cs
+++ Assets/Scripts/Interactable/PlayerInteraction.cs
@@ -4,19 +4,48 @@ using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private float range;
+    [SerializeField] Transform playerCamera;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void OnEnable() {
+
+        playerCamera = GetComponentInChildren<Camera>().transform;
+    }
+
+    private void OnDisable() {
+
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        //if(Input.GetButtonDown("Fire1")) {
+        if(Input.GetKeyDown(KeyCode.E)) {
+            Interact();
+        }   
     }
 
+
     void Interact() {
-        
+        Ray ray = new Ray();
+        RaycastHit hit;
+        ray.origin = playerCamera.position;
+        ray.direction = playerCamera.forward;
+        //Debug.Log(ray.origin);
+        //Debug.Log(ray.direction);
+        Debug.DrawRay(ray.origin,ray.direction * range, Color.red, 2f);
+        if(Physics.Raycast(ray,out hit ,range, layerMask )) {
+            //Debug.Log(hit.collider.name);
+            InteractionTarget target = hit.collider.GetComponent<InteractionTarget>();
+            if(target != null) {
+                target.Interact();
+            }
+        }
     }
 }
